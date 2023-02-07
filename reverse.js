3a628c76b6a1dbbd3fc4da3b1892e477b101f975
mdatiq1 <97389919+mdatiq1@users.noreply.github.com>
2023-02-07 13:07:28 +0530

    Add files via upload

---
+++ reverse.js
@@ -0,0 +1,15 @@
+// const reverse = (str)=>{
+//  let reverseStr= '';
+//   for(let char of str){
+//     reverseStr= char+reverseStr;
+//   }
+//   return console.log(reverseStr)
+// }
+
+// reverse('atiqur rahman')
+
+for (var i = 0; i < 5; i++) {     
+    setTimeout(function() {      
+        console.log(i)    
+     }, 0) }
+
